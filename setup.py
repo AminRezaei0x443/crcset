f0c97ca9c34f4b6465c475ff0d2e2cc35005fdef
Amin Rezaei <AminRezaei0x443@gmail.com>
2023-01-02 21:51:48 +0330

    feature: automated build flow

---
+++ setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+from distutils.errors import CompileError
+from subprocess import call
+from setuptools.command.build_py import build_py
+import shutil
+import os
+
+
+# Thanks to: https://www.ardanlabs.com/blog/2020/08/packaging-python-code.html
+class build_wasi_ext(build_py):
+    """Customized build command to build rust module as WASI."""
+
+    def run(self):
+        wasi_path = os.path.join(os.getcwd(), "py_crc32c/wasi/_crc32_wasi.wasm")
+        od = os.getcwd()
+        td = os.path.join(os.getcwd(), "crc32-rust")
+        os.chdir(td)
+        cmd = ["cargo", "wasi", "build", "--release"]
+        out = call(cmd)
+        os.chdir(od)
+        binary_path = os.path.join(
+            os.getcwd(), "crc32-rust/target/wasm32-wasi/release/crc32c.wasm"
+        )
+        shutil.copy(binary_path, wasi_path)
+        if out != 0:
+            raise CompileError("Rust WASI build failed")
+
+
+setup(
+    name="py-crc32c",
+    version="0.0.1",
+    description="",
+    license="MIT",
+    packages=find_packages(),
+    author="Amin Rezaei",
+    author_email="AminRezaei0x443@gmail.com",
+    keywords=[],
+    url="https://github.com/AminRezaei0x443/py-crc32c",
+    install_requires=[],
+    extras_require={},
+    package_data={
+        "py_crc32c.wasi": ["_crc32_wasi.wasm"],
+    },
+    cmdclass={"build_py": build_wasi_ext},
+    zip_safe=False,
+)
--- test/test.py
+++ test/test.py
@@ -8,17 +8,17 @@ store = Store(engine.Universal(Compiler))
 module = Module(store, wasm_bytes)
 wasi_version = wasi.get_version(module, strict=True)
 
-wasi_env = wasi.StateBuilder('wasi_test_program').finalize()
+wasi_env = wasi.StateBuilder("wasi_test_program").finalize()
 import_object = wasi_env.generate_import_object(store, wasi_version)
 instance = Instance(module, import_object)
 
 instance.exports._start()
 
-b_data = "madareto-gaiidam...".encode("utf-8")
+b_data = "test-string".encode("utf-8")
 pointer = instance.exports.alloc(len(b_data))
 memory = instance.exports.memory.uint8_view(offset=pointer)
 for i in range(len(b_data)):
     memory[i] = b_data[i]
 
-crc = instance.exports.crc32(pointer, len(b_data)) & 0xffffffff
+crc = instance.exports.crc32(pointer, len(b_data)) & 0xFFFFFFFF
 print(crc)
